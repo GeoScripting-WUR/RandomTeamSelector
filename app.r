91f3144f23623fef7b70f68280d6799cbaf7f13d
Dainius Masiliunas <pastas4@gmail.com>
2019-12-28 21:44:50 +0100

    Merge both apps into a unified random team generator app

--- app.r
+++ app.r
@@ -6,7 +6,6 @@ library(shiny)
 
 ## Update these values!
 ## ====================
-GroupSize = 3 # We want groups of 3 teams
 TeamNames = c("MMXIX",
 "Guardians of the Galaxy",
 "Mindwrap_Error",
@@ -42,7 +41,7 @@ TeamNames = c("MMXIX",
 "ArraysStartAt1",     ## Put initial names here
 "Mr_Robot"
 )
-TeamList = rep(list(TeamNames), 15) ## After there are any changes to teams over time, overwrite the slots like this:
+TeamList = rep(list(TeamNames), 16) ## After there are any changes to teams over time, overwrite the slots like this:
 ProjectTeams = c(
 "MMXIX",
 "Guardians of the Galaxy",
@@ -87,29 +86,45 @@ TeamList[c(8, 15)] = rep(list(ProjectTeams), 2)
 Year = 2020
 Seed = 0xfedbeef
 ## ====================
-# TODO: mention that the list is subject to change
 
-# Sanity check
+ExerciseGroupSize = 3 # We want groups of 3 teams
+AssignmentGroupSize = 2 # But only one for graded assignments
+
+DeliverableNames = c(paste("Exercise", 1:4),
+"Assignment 1",
+paste("Exercise", 5:8),
+"Assignment 2",
+paste("Exercise", 9:11),
+"Assignment 3",
+"Exercise 12",
+"Project")
+
+ExerciseIDs = grep("Exercise", DeliverableNames)
+FreeDays = which(DeliverableNames == "Exercise 8" | DeliverableNames == "Exercise 9")
+
+# Sanity checks
+stopifnot(length(DeliverableNames) == length(TeamList)) # We need one set of teams for each deliverable
 if (any(sapply(TeamList, anyDuplicated)))
     stop("Duplicate names found!")
 
+names(TeamList) = DeliverableNames
+
 # Web UI
 ui = fluidPage(
-  titlePanel("Geoscripting discussion group generator"),
+  titlePanel("Geoscripting review group generator"),
   sidebarLayout(
     sidebarPanel(
-      helpText("This Shiny app tells you which teams you should go to in order to discuss the answers to the exercise of the day."),
-      tags$div(class="header", checked=NA,
-               tags$a(href="https://gist.github.com/GreatEmerald/a5e7ed83ab16c0125ca11079adfbaf50", "Source code")
-      ),
-      sliderInput("Assignment",
-                  "Exercise number:",
-                  min = 1,
-                  max = 15,
-                  value = 1),
+      tags$p("This Shiny app tells you which teams you should go to in order to discuss the answers to the exercise of the day, or which team to review for the assignments of the week.",
+      tags$b("Please do not write these down as teams may change throughout the course! Always rerun this app using runGist()!")),
+      tags$p(""),
+      selectInput("DeliverableName", label = "Assignment or exercise to review:", choices=DeliverableNames),
       selectInput("MyTeam", 
-        label = "Your team name",
-        choices = c("SELECT YOUR TEAM NAME", TeamList[[1]]))
+        label = "Your team name:",
+        choices = c("SELECT YOUR TEAM NAME", TeamList[[1]])),
+      helpText("Hint: if you click on the boxes above and press Backspace, you can search for your team name by typing it."),
+      tags$div(class="header", checked=NA,
+               tags$a(href="https://gist.github.com/GreatEmerald/a5e7ed83ab16c0125ca11079adfbaf50", "Source code of this Shiny app")
+      )
     ),
 
     mainPanel(
@@ -122,24 +137,24 @@ ui = fluidPage(
 server = function(input, output, session)
 {
   output$YourTeam <- renderText({
-    updateSelectInput(session, "MyTeam", choices=c("SELECT YOUR TEAM NAME", TeamList[[input$Assignment]]), selected=ifelse(input$MyTeam %in% TeamList[[input$Assignment]], input$MyTeam, "SELECT YOUR TEAM NAME"))
+    DeliverableID = which(DeliverableNames == input$DeliverableName) # Alternatively could use TeamList[[input$DeliverableName]] everywhere
+    updateSelectInput(session, "MyTeam", choices=c("SELECT YOUR TEAM NAME", TeamList[[DeliverableID]]), selected=ifelse(input$MyTeam %in% TeamList[[DeliverableID]], input$MyTeam, "SELECT YOUR TEAM NAME"))
     
     if (input$MyTeam == "SELECT YOUR TEAM NAME")
-        "Please select the exercise number by sliding the slider on the left, and then select your team name from the drop-down box."
-    else if (input$Assignment == 10)
-        "This exercise is on DataCamp, no need to discuss it!"
-    else if (input$Assignment == 13 || input$Assignment == 14)
-        "No exercise for today either, so you do not need to discuss anything."
+        "Please select the exercise or assignment number by using the first drop-down box, and then select your team name from the second drop-down box."
+    else if (DeliverableID %in% FreeDays)
+        "There is no exercise that needs to be reviewed today."
     else
     {
-        TeamNames = TeamList[[input$Assignment]]
+        GroupSize = if (DeliverableID %in% ExerciseIDs) ExerciseGroupSize else AssignmentGroupSize
+        TeamNames = TeamList[[DeliverableID]]
         names(TeamNames) = TeamNames
         
         if (!input$MyTeam %in% TeamNames)
-            paste0("Your team (", input$MyTeam, ") was not registered to exercise ", input$Assignment, ", please contact the course coordinators to check if you should participate in the discussions.")
+            paste0("Your team (", input$MyTeam, ") was not registered to ", input$DeliverableName, ", please contact the course coordinators to check if you should review or participate in the discussions.")
         else
         {
-            set.seed(input$Assignment+Year+Seed)
+            set.seed(DeliverableID+Year+Seed)
             Weights = runif(length(TeamNames))
             # names(weights) = TeamNames
             GroupNumbers = ceiling(order(Weights) / GroupSize)
@@ -149,9 +164,9 @@ server = function(input, output, session)
             stopifnot(length(MyGroup) == 1)
         
             switch(length(OtherTeams)+1,
-                paste0("For exercise ", input$Assignment, ", your team (", input$MyTeam, ") is all alone! You can discuss with staff or join whatever group you prefer."),
-                paste0("For exercise ", input$Assignment, ", your team (", input$MyTeam, ") should discuss your answers with team \"", OtherTeams[1], "\"."),
-                paste0("For exercise ", input$Assignment, ", your team (", input$MyTeam, ") should discuss your answers with team \"", OtherTeams[1], "\" and \"", OtherTeams[2], "\".")
+                paste0("For ", input$DeliverableName, ", your team (", input$MyTeam, ") is all alone! You can discuss with staff or review whatever group you prefer."),
+                paste0("For ", input$DeliverableName, ", your team (", input$MyTeam, ") should review the work of team \"", OtherTeams[1], "\"."),
+                paste0("For ", input$DeliverableName, ", your team (", input$MyTeam, ") should discuss your answers with teams \"", OtherTeams[1], "\" and \"", OtherTeams[2], "\".")
             )
         }
     }
